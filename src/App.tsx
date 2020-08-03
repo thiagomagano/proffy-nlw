402727669fd6e45e38c34d9a4dc994dd44ed4c18
Thiago Magano <thiagomagano1993@outlook.com>
2020-08-03 15:06:22 -0300

    Definido Estilos Globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  let [count, setCount] = useState(1);
+
+  function handleCount() {
+    setCount(count * 2);
+  }
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => handleCount()}>Aumentar</button>
+    </div>
+  );
 }
 
 export default App;
