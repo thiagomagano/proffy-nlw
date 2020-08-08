e3a252ed6dae414c3af305cebcc1eebd9430573b
Thiago Magano <thiagomagano1993@outlook.com>
2020-08-08 16:18:48 -0300

    modify api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL:
+    "https://proffy-server-nlw2.herokuapp.com" || "http://localhost:3333",
 });
 
 export default api;
